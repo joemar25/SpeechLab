e8f41cb244c576e6b8ed6c685f57f51108cdfd66
SeanPalacay <palacaysean@example.com>
2024-07-27 07:33:56 +0800

    project structure

---
+++ src/app/features/course/teacher-mode/teacher-mode.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-mode',
+  standalone: true,
+  imports: [],
+  templateUrl: './teacher-mode.component.html',
+  styleUrl: './teacher-mode.component.css'
+})
+export class TeacherModeComponent {
+
+}
