2b1fd1348a0d965c94e892c05d3b9c42bdc3ed90
Alro John <alromercado08@gmail.com>
2024-07-27 10:59:42 +0800

    added student-layout component, added a components for student layout, added design on  header and sidebar

---
+++ src/app/student-layout/class-lab/class-lab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-lab',
+  standalone: true,
+  imports: [],
+  templateUrl: './class-lab.component.html',
+  styleUrl: './class-lab.component.css'
+})
+export class ClassLabComponent {
+
+}
--- src/app/student-layout/student-layout.component.ts
+++ src/app/student-layout/student-layout.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { SideBarComponent } from "./components/side-bar/side-bar.component";
 
 @Component({
   selector: 'app-student-layout',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, HeaderComponent, SideBarComponent, ],
   templateUrl: './student-layout.component.html',
   styleUrl: './student-layout.component.css'
 })
