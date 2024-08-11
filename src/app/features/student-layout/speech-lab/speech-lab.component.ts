7ac107be3f12766b86b6819436ab5077345e13bd
SeanPalacay <palacaysean@example.com>
2024-08-11 09:18:35 +0800

    text to speech layout

---
+++ src/app/features/student-layout/speech-lab/speech-lab.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-speech-lab',
+  standalone: true,
+  imports: [],
+  templateUrl: './speech-lab.component.html',
+  styleUrl: './speech-lab.component.css'
+})
+export class SpeechLabComponent {
+
+}
