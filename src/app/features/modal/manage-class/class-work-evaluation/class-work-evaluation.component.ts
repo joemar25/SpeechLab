80ed0f142d5bcb458c0a2d1c6de0fe6f934524e2
Alro John <alromercado08@gmail.com>
2024-08-25 00:19:30 +0800

    added manage-class students,

--- src/app/features/modal/manage-class/class-work-evaluation/class-work-evaluation.component.ts
+++ src/app/features/modal/manage-class/class-work-evaluation/class-work-evaluation.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, CUSTOM_ELEMENTS_SCHEMA, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-class-work-evaluation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './class-work-evaluation.component.html',
   styleUrl: './class-work-evaluation.component.css'
